42f71e2f68ec6fb7089be6902cf19daa3e891fea
teakaysea <teakaysea@gmail.com>
2021-10-09 09:50:33 +0900

    check method and set status code

--- main.go
+++ main.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Method: %s", r.Method)
+	switch r.Method {
+	case "GET":
+		log.Printf("implementation for GET")
+	case "POST":
+		log.Printf("implementation for POST")
+	default:
+		log.Printf("%s not allowed", r.Method)
+	}
+	w.WriteHeader(http.StatusOK)
 	fmt.Fprintln(w, "Hello, world!!")
 }
 func main() {
