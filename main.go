9da82619d9626a06ff0910f743d3a0210a6686f4
teakaysea <teakaysea@gmail.com>
2021-10-11 22:32:28 +0900

    temporary modification

--- main.go
+++ main.go
@@ -6,7 +6,18 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+var handlerMap map[string]handler
+
+type handler struct {
+	path   string
+	method string
+	f      http.HandlerFunc
+}
+
+func (h handler) key() string {
+	return fmt.Sprintf("%s %s", h.method, h.path)
+}
+func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Method: %s", r.Method)
 	switch r.Method {
 	case "GET":
@@ -19,7 +30,22 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintln(w, "Hello, world!!")
 }
+func init() {
+	handlerMap = map[string]handler{}
+}
 func main() {
-	http.HandleFunc("/", handler)
+	addRoute(http.MethodGet, "/", hello)
+
 	http.ListenAndServe(":8080", nil)
+}
+func newHandler(path, method string, f http.HandlerFunc) handler {
+	return handler{path: path, method: method, f: f}
+}
+func addRoute(path, method string, f http.HandlerFunc) {
+	h := newHandler(path, method, f)
+	handlerMap[h.key()] = h
+	http.HandleFunc(path, getHandler(h))
+}
+func getHandler(h handler) http.HandlerFunc {
+	return nil
 }
