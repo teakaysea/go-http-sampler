d896efb90c34f3e3df15224c2aec6b80c1254c6a
teakaysea <teakaysea@gmail.com>
2021-10-13 08:58:47 +0900

    add context

--- main.go
+++ main.go
@@ -7,26 +7,56 @@ import (
 
 type handler struct {
 	method string
-	f      http.HandlerFunc
+	f      handlerFunc
+}
+type engine struct {
+}
+type context struct {
+	w http.ResponseWriter
+	r *http.Request
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintln(w, "Hello, world!!")
+type handlerFunc func(*context)
+
+func newEngine() *engine {
+	return new(engine)
+}
+
+func newContext(w http.ResponseWriter, r *http.Request) *context {
+	c := new(context)
+	c.w = w
+	c.r = r
+	return c
+}
+func (c *context) status(code int) {
+	c.w.WriteHeader(code)
+}
+func (c *context) writeBody(str string) {
+	fmt.Fprintln(c.w, str)
+}
+func hello(c *context) {
+	c.status(http.StatusOK)
+	c.writeBody("Hello, world!!")
 }
 func main() {
-	handleFunc("/hello", http.MethodGet, hello)
+	e := newEngine()
+	e.GET("/hello", hello)
 
 	http.ListenAndServe(":8080", nil)
 }
-func handleFunc(path, method string, f http.HandlerFunc) {
+
+func (e *engine) handleFunc(path, method string, f handlerFunc) {
 	h := handler{method: method, f: f}
 	http.HandleFunc(path, h.handlerFunc)
+}
+func (e *engine) GET(path string, f handlerFunc) {
+	e.handleFunc(path, http.MethodGet, f)
 }
 func (h handler) handlerFunc(w http.ResponseWriter, r *http.Request) {
 	if h.method != r.Method {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	h.f(w, r)
+	c := newContext(w, r)
+	h.f(c)
 }
